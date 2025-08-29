94d7d2d2879ea88f64bf6ade4331ba41fa322acb
negiisha799-glitch <negiisha799@gmail.com>
2025-08-29 12:03:07 +0530

    q1 sum of two numbers.c

---
+++ q1 sum of two numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int number1,number2,result;
+    printf("Enter the number 1\n");
+    scanf("%d",&number1);
+    printf("Enter the number 2\n");
+    scanf("%d",&number2)
+   result = number1 + number2;
+   printf("%d + %d = %d",number1,number2,result);
+   return 0;
+}
